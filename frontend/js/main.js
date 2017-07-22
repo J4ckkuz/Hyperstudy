75afa0e0c0ca8fcacc2fae67e16a7e7f22b3662c
Jack <J4ckkuz@gmail.com>
2017-07-22 16:41:54 +0800

    Build 0.2.0
    Add functionality to generate cards from JSON
    Fix borders not displaying properly
    Separate card node creation from Card constructor

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,11 +1,7 @@
-/*
-I feel like I might be going about this the wrong way. This code combines the DOM
-elements and JS objects. What might be better is having a model to represent the
-information, then doing something to render it onto the page.
-*/
 var hyperstudy = (function () {
     "use strict";
 
+    var Delta = Quill.import('delta');
     /*
         Basic variables
     */
@@ -15,23 +11,14 @@ var hyperstudy = (function () {
         cards: []
     };
 
-    // cue = cardList[i].firstChild
-    // notes = cardList[i].firstChild.nextSibling
-
-
-    // This is a purely logical representation of a simple note sheet.
-    // Similar to what will be sent to the database. It only contains data,
-    // no references to objects. The DOM should follow this, not the other way round.
-    // However, we still need a way to access DOM objects for Quill purposes and such.
-
-    // Simulated JSON from server
+    // Simulated JSON from server. Should actually have deltas instead of strings
     var exampleSheet = {
         name: "Biology 101",
         date: Date.now(),
         cards: [
             {
                 cue: "Mitochondria",
-                lines: [
+                notes: [
                     "Mitochondria are rod-shaped organelles",
                     "Convert O2, nutrients to ATP",
                     "This is called aerobic respiration",
@@ -39,10 +26,9 @@ var hyperstudy = (function () {
                     "Enable cells to produce 15x more ATP"
                 ],
                 // A reference to the DOM object. This should be added by the client when cards are added, not included in the JSON or stored in the database.
-                element: document.getElementsByClassName("card")[0]
             },{
                 cue: "What is a cell membrane?",
-                lines: [
+                notes: [
                     "Thin, semi-permeable membrane",
                     "Surrounds cytoplasm of cell",
                     "Protects interior of cell",
@@ -52,7 +38,6 @@ var hyperstudy = (function () {
         ]
     };
     var exampleJSON = JSON.stringify(exampleSheet);
-    var parsedJSON = JSON.parse(exampleJSON);
 
     /*
         Utility functions
@@ -64,68 +49,126 @@ var hyperstudy = (function () {
         element.classList.add(type);
         return element;
     };
-    // Might make this into a constructor or delete it.
 
-    var Card = function(sheet) {
-        // Create a card DOM object
-        var card = createAndClass("card");
-        var cue = createAndClass("cue");
-        var notes = createAndClass("notes");
-        // create Quill editors for cue and notes
-        var cueEditor = new Quill(cue);
-        var notesEditor = new Quill(notes);
-        // Add references to the Card object for access to editors.
-        this.cue = cueEditor;
-        this.notes = notesEditor;
+    // Don't really need this
+    var updateProperty = function(editor, property, that) {
+        that[property] = editor.getContents();
+    };
+
+/*
+A word on the database/storage method:
+Store both the Delta (for editor/viewing usage) and the plaintext form (for revision usage).
+Not sure yet where the Delta wil be processed. Write a function for it anyway, since it'll be used somewhere.
+
+
+Card {
+    cueEditor:
+    cueDelta:
+    cueText:
+    notesEditor:
+    notesDelta:
+    notesText:
+}
+*/
+    var paramExists = function(parameter) {
+        if (parameter !== undefined) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    var createCardNode = function() {
+        var cardNode = document.createElement("section");
+        cardNode.classList.add("card");
+        var cueNode = createAndClass("cue");
+        var notesNode = createAndClass("notes");
+        cardNode.appendChild(cueNode);
+        cardNode.appendChild(notesNode);
+        return {card: cardNode, cue: cueNode, notes: notesNode};
+    };
+
+    var toDelta = function(text) {
+        var delta = new Delta();
+        if (typeof text === 'string') {
+            delta.insert(text);
+        }
+        if (text.constructor === Array) {
+            var joinedArray = text.join("\n");
+            delta.insert(joinedArray);
+        }
+        return delta;
+    };
+
+    var Card = function(sheet, cueDelta, notesDelta) {
         sheet.cards.push(this);
+        var thisCard = this;
 
-        card.appendChild(cue);
-        card.appendChild(notes);
-        root.appendChild(card);
+        // Create a card DOM object
+        var node = createCardNode();
+        root.appendChild(node.card);
+
+        // create Quill editors for cue and notes
+        var cueEditor = new Quill(node.cue);
+        var notesEditor = new Quill(node.notes);
+
+        // Add references to editors.
+        this.cueEditor = cueEditor;
+        this.notesEditor = notesEditor;
+
+        // Are these necessary?
+        cueEditor.on("text-change", function() {
+            thisCard.cue = cueEditor.getContents();
+        });
+        notesEditor.on("text-change", function() {
+            thisCard.notes = notesEditor.getContents();
+        });
+
+        // Set editor contents
+        if (paramExists(cueDelta)) {
+            cueEditor.setContents(cueDelta);
+        }
+        if (paramExists(notesDelta)) {
+            notesEditor.setContents(notesDelta);
+        }
     };
-    Card.prototype.update = function() {
-        // Call this when the text in the editor is changed.
 
-    };
-
-    // TODO Test. Will be called when JSON model of Sheet arrives from server
-    var loadSheet = function(JSONString) {
-        // First create the data/logical object
-        var sheet = JSON.parse(JSONString);
+    var loadSheet = function(JSONresponse) {
+        var sheet = JSON.parse(JSONresponse);
         // Create cards for every card in the sheet JSON
         for (var i = 0; i < sheet.cards.length; i++) {
-            // Placeholder functions
-            new Card();
-            addEditor();
-            populateEditor();
+            // later, the toDelta() will not be needed as the JSON will contain Deltas.
+            new Card(testSheet, toDelta(sheet.cards[i].cue), toDelta(sheet.cards[i].notes));
         }
 
     };
-
-
     /*
         Tests
     */
+
+    var testDelta = toDelta("Test");
+    var testDelta2 = toDelta(["Makes 2 assumptions:", "Atoms have fixed sizes", "Atoms are hard and not deformable"]);
+
     document.getElementById("cardAdder").addEventListener("click", function () {
-        new Card(testSheet, "Hard sphere model", ["Makes 2 assumptions:", "Atoms have fixed sizes", "Atoms are hard and not deformable"]);
+        new Card(testSheet, testDelta, testDelta2);
     });
     document.getElementById("accessTester").addEventListener("click", function () {
         console.log(testSheet);
     });
-    document.getElementById("JSONTest").addEventListener("click", function () {
-
+    document.getElementById("test1").addEventListener("click", function () {
+        loadSheet(exampleJSON);
+    });
+    document.getElementById("test2").addEventListener("click", function () {
 
     });
-
 
 
 
     return {
         // function funcName() {
-        "addCard": addCard
+
         // };
     };
-
 })();
 
 // Keyboard behaviour
